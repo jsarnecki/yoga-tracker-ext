0b254d693f78d31ba2003f1065e8fe0247318aa0
Joshyjosh <joshsarnecki@gmail.com>
2022-06-22 14:33:17 -0700

    Getting database set up, testing queries

--- db-test.js
+++ db-test.js
@@ -1,5 +1,7 @@
 const { Client } = require('pg');
 
+const args = process.argv.slice(2);
+
 const client = new Client({
   host: 'localhost',
   user: 'joshyjosh',
@@ -10,12 +12,28 @@ const client = new Client({
 
 client.connect();
 
-client.query('SELECT * FROM users', (err, res) => {
-  if (err) {
-    console.log('error:', err);
-    client.end();
-  }
+switch(args[0]) {
+  case 'users':
+    client.query('SELECT * FROM users', (err, res) => {
+      if (err) {
+        console.log('error:', err);
+        client.end();
+      }
+      console.log('res.rows:', res.rows);
+      client.end();
+    });
+    break;
+  default:
+    return client.query('INSERT INTO users(name, age) VALUES ($1, $2);', [args[0], args[1]])
+      .then((result) => {
+        console.log('Successfully inserted');
+        client.end();
+      })
+      .catch((err) => {
+        console.log('Caught err:', err);
+        client.end();
+      });
+}
 
-  console.log('res.rows:', res.rows);
-  client.end();
-})
+
+
